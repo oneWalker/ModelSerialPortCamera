cf5e88eb660303c1ddd7442789db8b0bc7fa65ee
oneWalker <briankenliu@gmail.com>
2018-12-04 23:07:12 +0800

    achieved projects

---
+++ CDD/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace CDD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.chuankou = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bote = new System.Windows.Forms.ComboBox();
+            this.jiaoyan = new System.Windows.Forms.ComboBox();
+            this.shuju = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tingzhi = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chuankou
+            // 
+            this.chuankou.FormattingEnabled = true;
+            this.chuankou.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5"});
+            this.chuankou.Location = new System.Drawing.Point(97, 19);
+            this.chuankou.Name = "chuankou";
+            this.chuankou.Size = new System.Drawing.Size(121, 20);
+            this.chuankou.TabIndex = 0;
+            this.chuankou.Text = "COM1";
+            this.chuankou.SelectedIndexChanged += new System.EventHandler(this.chuankou_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "串口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "波特率";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "校验位";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "数据位";
+            // 
+            // bote
+            // 
+            this.bote.FormattingEnabled = true;
+            this.bote.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.bote.Location = new System.Drawing.Point(97, 45);
+            this.bote.Name = "bote";
+            this.bote.Size = new System.Drawing.Size(121, 20);
+            this.bote.TabIndex = 6;
+            this.bote.Text = "38400";
+            // 
+            // jiaoyan
+            // 
+            this.jiaoyan.FormattingEnabled = true;
+            this.jiaoyan.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even",
+            "Mark",
+            "Space"});
+            this.jiaoyan.Location = new System.Drawing.Point(97, 73);
+            this.jiaoyan.Name = "jiaoyan";
+            this.jiaoyan.Size = new System.Drawing.Size(121, 20);
+            this.jiaoyan.TabIndex = 7;
+            this.jiaoyan.Text = "None";
+            // 
+            // shuju
+            // 
+            this.shuju.FormattingEnabled = true;
+            this.shuju.Items.AddRange(new object[] {
+            "8",
+            "7",
+            "6"});
+            this.shuju.Location = new System.Drawing.Point(97, 100);
+            this.shuju.Name = "shuju";
+            this.shuju.Size = new System.Drawing.Size(121, 20);
+            this.shuju.TabIndex = 8;
+            this.shuju.Text = "8";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "停止位";
+            // 
+            // tingzhi
+            // 
+            this.tingzhi.FormattingEnabled = true;
+            this.tingzhi.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.tingzhi.Location = new System.Drawing.Point(97, 125);
+            this.tingzhi.Name = "tingzhi";
+            this.tingzhi.Size = new System.Drawing.Size(121, 20);
+            this.tingzhi.TabIndex = 10;
+            this.tingzhi.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(35, 219);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 16);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "160*120";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(35, 251);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 16);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.Text = "320*240";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(35, 283);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 16);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.Text = "640*490";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "packet size";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 348);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "80";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "拍照";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(526, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 345);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(225, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "图片大小";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(321, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(227, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "分包数量";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(321, 260);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(687, 409);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "显示图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 478);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bote);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chuankou);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tingzhi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.shuju);
+            this.Controls.Add(this.jiaoyan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox chuankou;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox bote;
+        private System.Windows.Forms.ComboBox jiaoyan;
+        private System.Windows.Forms.ComboBox shuju;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox tingzhi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CDD/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace CDD
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort sp = new SerialPort();
+        int NumPack;            //包数
+        int ImageSize;          //图片大小
+        public SortedList<int, byte[]> ReliablephotoDataListOne = new SortedList<int, byte[]>();//用于存储二进制数据
+        int packetsize=0;//每包的大小
+        int count=0;
+        string curFileNameOne;
+        FileStream fs1;
+        public delegate void TxtAppendHandler(string msg);
+
+
+/*        public void writeFile(string content)
+        {
+            if (content.Substring(0, 14) == "55 48 23 55 52".Substring(0, 14))
+            {
+                string result1, result2, result3, result4;
+                result1 = content.Substring(27, 2);
+                result2 = content.Substring(30, 2);
+                NumPack = Convert.ToInt32(result2 + result1, 16);
+                textBox3.Text += ("分包数量:" + NumPack + "\n");
+
+                result1 = content.Substring(15, 2);
+                result2 = content.Substring(18, 2);
+                result3 = content.Substring(21, 2);
+                result4 = content.Substring(24, 2);
+
+                ImageSize = Convert.ToInt32(result4 + result3 + result2 + result1, 16);
+                textBox2.Text += ("图片大小:" + ImageSize + "字节\n");
+
+                textBox3.Text = NumPack.ToString();
+                textBox2.Text = ImageSize.ToString();
+            }
+        }
+*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化SerialPort对象
+            sp.NewLine = "/r/n";
+            sp.RtsEnable = true;
+            //添加事件注册
+            sp.DataReceived += sp_DataReceived;
+        }
+
+
+
+
+        public void setText(string msg)
+        {//显示数据包总数和图像大小
+            string[] data = msg.Split(' ');
+            textBox3.Text = data[0];//packetCount.ToString();
+            textBox2.Text = data[1];
+            NumPack = Convert.ToInt32(data[0]);
+            ImageSize = Convert.ToInt32(data[1]);
+        }
+        /// <summary>
+        /// 串口接收数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sp_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(100);//延时1ms，因为一次串口发送的最大数据包是80字节，在波特率为38400时，串口传输时间大概为17ms,再加上处理时间，不会超过20ms.
+
+            byte[] imageData = new byte[packetsize + 9];
+            byte[] packet = new byte[packetsize];
+            byte[] command = new byte[5];
+            byte[] commandImgInfo = new byte[12];
+            command[0] = Convert.ToByte(sp.ReadByte());
+            command[1] = Convert.ToByte(sp.ReadByte());
+            if (command[0] == 'U' && command[1] == 'H')
+            {//R数据帧1个字节(’U’) +1个字节(’R’) + 4个字节(图像大小) + 2个字节(分包数量) +1个字节(’#’)；
+                sp.Read(commandImgInfo, 0, 10);
+                NumPack = (commandImgInfo[8] << 8) | commandImgInfo[7];//分包数量
+                ImageSize = (commandImgInfo[6] << 24) | (commandImgInfo[5] << 16) | (commandImgInfo[4] << 8) | commandImgInfo[3];//图像大小
+
+                TxtAppendHandler deltxt = new TxtAppendHandler(setText);//使用委托机制处理线程问题
+                this.Invoke(deltxt, new object[] { NumPack.ToString() + " " + ImageSize.ToString() });
+            }
+            else if (command[0] == 'U' && command[1] == 'E')
+            {//F数据帧1个字节(’U’) +1 个字节(’F’) + 2个字节(包号) + 2个字节(包长度) + N 个字节(图像数据) + 2个字节(校验和)；
+                sp.Read(imageData, 0, packetsize + 9);
+                count = (imageData[4] << 8) | (imageData[3]);
+            //    textBox4.Text = Convert.ToString(count);
+                for (int i = 7; i < imageData.Length - 2; i++)
+                {
+                    packet[i - 7] = imageData[i];
+                }
+                if (count <= NumPack)
+                {
+                    ReliablephotoDataListOne.Add(count, packet);//将每次的数据包都存储起来
+                }
+     //           TxtAppendHandler deltxt = new TxtAppendHandler(showCountText);//使用委托机制处理线程问题
+     //           this.Invoke(deltxt, new object[] { count.ToString() });
+            }
+            else
+            {//不做处理
+            }
+            if (count == NumPack)
+            { 
+                try
+                {
+                    curFileNameOne = GblDefine.curPicPathName + "\\" + DateTime.Now.ToString("yyyy-M-d H-m-s") + ".jpg";
+
+                    fs1 = new FileStream(curFileNameOne, FileMode.Create);//存储图片
+                    BinaryWriter w1 = new BinaryWriter(fs1);
+
+                    for (int i = 1; i <= ReliablephotoDataListOne.Count; i++)
+                    {
+                        w1.Write(ReliablephotoDataListOne[i]);
+                    }
+                    w1.Flush();
+                    fs1.Flush();
+                    w1.Close();
+                    fs1.Close();
+                    FileInfo fi1 = new FileInfo(curFileNameOne);
+                    if (fi1.Length == 0)
+                    {
+                        MessageBox.Show("Image collect failed！");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Image collect end！");
+                        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                        pictureBox1.Image = Image.FromFile(curFileNameOne, false);
+                    }
+                    ReliablephotoDataListOne.Clear();//清空数据
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void showCountText(string msg)
+        {//显示目前已经获取到的数据包数量
+     //       nodeNum.Text = msg;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chuankou_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sp.PortName = chuankou.Text;//设置端口号
+            sp.BaudRate = Convert.ToInt32(bote.Text);//设置比特率
+            sp.DataBits = Convert.ToInt32(shuju.Text);//数据位
+            sp.Parity = (Parity)Enum.Parse(typeof(Parity), jiaoyan.SelectedItem.ToString());//设置奇偶校验位
+            switch (tingzhi.Text)//停止位
+            {
+                case "1":
+                    sp.StopBits = StopBits.One;
+                    break;
+                case "2":
+                    sp.StopBits = StopBits.Two;
+                    break;
+            }
+            try
+            {
+                sp.Open();
+                sp.RtsEnable = true;
+                MessageBox.Show("串口已打开！");
+            }
+            catch (Exception ex)
+            {
+                //tssStatus.Text = "串口状态：打开串口失败！";
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 字符串转16进制字节数组
+        /// </summary>
+        /// <param name="hexString"></param>
+        /// <returns></returns>
+        private static byte[] GetBytes(string hexString)
+        {
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0)
+                hexString += " ";
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(textBox1.Text);
+            packetsize = size;
+            string stemp = Convert.ToString(size, 16);
+            string temp = null;
+            byte[] Image1 = { 0x55, 0x48, 0x31, 0x00, 0x00, 0x23 };//160*128像素图像
+            byte[] Image2 = { 0x55, 0x48, 0x32, 0x00, 0x00, 0x23 };//320*240像素图像
+            byte[] Image3 = { 0x55, 0x48, 0x33, 0x00, 0x00, 0x23 };//640*480像素图像
+            if (stemp.Length == 1)
+            {
+                temp = "0" + stemp;
+            }
+            else
+            {
+                temp = stemp;
+            }
+            byte[] ps = GetBytes(temp);
+            if (radioButton1.Checked)
+            {
+                Image1[3] = ps[0];//设置数据分包大小
+                if (size > 255)
+                {
+                    Image1[4] = ps[1];
+                }
+                sp.Write(Image1, 0, Image1.Length);//发送采集命令
+            }
+            else if (radioButton2.Checked)
+            {
+                Image2[3] = ps[0];//设置数据分包大小
+                if (size > 255)
+                {
+                    Image2[4] = ps[1];
+                }
+                sp.Write(Image2, 0, Image2.Length);//发送采集命令
+            }
+            else if (radioButton3.Checked)
+            {
+                Image3[3] = ps[0];//设置数据分包大小
+                if (size > 255)
+                {
+                    Image3[4] = ps[1];
+                }
+                sp.Write(Image3, 0, Image3.Length);//发送采集命令
+            }
+            else
+            {
+
+            }
+            Thread.Sleep(150);//摄像头接到拍图命令后，需耗时 T p ，该延时最短为150ms
+        }
+
+
+
+       
+
+        public void getImagePacket()
+        {//得到图像的所有分包
+            for (int i = 1; i <= NumPack; i++)
+            {
+                string temp = Convert.ToString(i, 16);
+                string stemp = null;
+                if (temp.Length == 1)
+                {
+                    stemp = "000" + temp;
+                }
+                else if (temp.Length == 2)
+                {
+                    stemp = "00" + temp;
+                }
+                else if (temp.Length == 3)
+                {
+                    stemp = "0" + temp;
+                }
+                else
+                {
+                    stemp = temp;
+                }
+                byte[] packetID = GetBytes(stemp);
+                byte[] Require_Image = { 0x55, 0x45, 0x01, 0x00, 0x23 };//请求图像数据包
+                Require_Image[2] = packetID[1];
+                Require_Image[3] = packetID[0];
+
+                sp.Write(Require_Image, 0, Require_Image.Length);//发送采集命令
+                Thread.Sleep(packetsize * 8 / sp.BaudRate + 100);//每次至少延时每个数据包的最小传送时间
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread th1 = new Thread(new ThreadStart(getImagePacket));
+            th1.Start();
+        }
+
+        
+
+
+
+
+
+
+    }
+    class GblDefine
+    {
+        public static string curPicPathName = "d:\\";
+    }
+}
